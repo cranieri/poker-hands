7d861e259a1c60e1894c408ee70b4aa4ec3e6a5c
Cosimo Ranieri <co.ranieri@gmail.com>
2013-09-02 09:45:22 +0200

    refactoring

--- lib/deck/hand.rb
+++ lib/deck/hand.rb
@@ -1,7 +1,6 @@
 require 'deck/rank_selector'
 module Deck
 class Hand
-  include ::Deck::RankSelector
   include Comparable
 
   attr_reader :cards
@@ -12,26 +11,14 @@ class Hand
   end
 
   def only_values
-    @cards.map { |card| card.value}
+    cards.map { |card| card.value}
   end
 
   def rank
-    @rank ||= select_rank
+    @rank ||= RankSelector.new(self).select_rank
   end
 
   private
-
-  def select_rank
-    return Rank::StraightFlush.new(self) if is_straight_flush?
-    return Rank::FourOfAKind.new(self) if is_four_of_a_kind?
-    return Rank::FullHouse.new(self) if is_full_house?
-    return Rank::Flush.new(self) if is_flush?
-    return Rank::Straight.new(self) if is_straight?
-    return Rank::ThreeOfAKind.new(self) if is_three_of_a_kind?
-    return Rank::TwoPairs.new(self) if is_two_pairs?
-    return Rank::Pair.new(self) if is_pair?
-    Rank::HighCard.new(self)
-  end
 
   def <=>(hand)
     self.rank <=> hand.rank
--- lib/deck/rank_selector.rb
+++ lib/deck/rank_selector.rb
@@ -1,13 +1,18 @@
 module Deck
-module RankSelector
+class RankSelector
+  attr_reader :hand
+
+  def initialize(hand)
+    @hand = hand
+  end
 
   def is_pair?
-    only_value = @cards.map { |card| card.value}
+    only_value = hand.cards.map { |card| card.value}
     only_value.detect { |card| only_value.count(card) > 1}
   end
 
   def is_two_pairs?
-    only_value = @cards.map { |card| card.value}
+    only_value = hand.cards.map { |card| card.value}
     first_value = only_value.detect { |card| only_value.count(card) == 2}
     if first_value
       only_value.delete(first_value)
@@ -17,23 +22,23 @@ module RankSelector
   end
 
   def is_three_of_a_kind?
-    only_value = @cards.map { |card| card.value}
+    only_value = hand.cards.map { |card| card.value}
     only_value.detect { |card| only_value.count(card) == 3}
   end
 
   def is_straight?
-    only_value = @cards.map { |card| card.value}
+    only_value = hand.cards.map { |card| card.value}
     sorted_only_value = only_value.sort
     only_value === sorted_only_value
   end
 
   def is_flush?
-    only_suits = @cards.map { |card| card.suit}
+    only_suits = hand.cards.map { |card| card.suit}
     only_suits.detect { |card| only_suits.count(card) == 5}
   end
 
   def is_full_house?
-    only_value = @cards.map { |card| card.value}
+    only_value = hand.cards.map { |card| card.value}
     first_value = only_value.detect { |card| only_value.count(card) == 3}
     if first_value
       only_value.delete(first_value)
@@ -43,17 +48,29 @@ module RankSelector
   end
 
   def is_four_of_a_kind?
-    only_value = @cards.map { |card| card.value}
+    only_value = hand.cards.map { |card| card.value}
     only_value.detect { |card| only_value.count(card) == 4}
   end
 
   def is_straight_flush?
-    only_suits = @cards.map { |card| card.suit}
+    only_suits = hand.cards.map { |card| card.suit}
     all_same_suit = only_suits.detect { |card| only_suits.count(card) == 5}
 
-    only_value = @cards.map { |card| card.value}
+    only_value = hand.cards.map { |card| card.value}
     sorted_only_value = only_value.sort
     all_same_suit && only_value === sorted_only_value
+  end
+
+  def select_rank
+    return Rank::StraightFlush.new(hand) if is_straight_flush?
+    return Rank::FourOfAKind.new(hand) if is_four_of_a_kind?
+    return Rank::FullHouse.new(hand) if is_full_house?
+    return Rank::Flush.new(hand) if is_flush?
+    return Rank::Straight.new(hand) if is_straight?
+    return Rank::ThreeOfAKind.new(hand) if is_three_of_a_kind?
+    return Rank::TwoPairs.new(hand) if is_two_pairs?
+    return Rank::Pair.new(hand) if is_pair?
+    Rank::HighCard.new(hand)
   end
 end
 end
